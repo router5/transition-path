723653e16902189dbf334dc3f29981c347fe5d49
Thomas Roch <thomas.roch@fanduel.com>
2015-10-26 11:17:20 +0000

    chore: add transitionPath and tests

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # transition-path
-Router5 helper to determine a transition path
+
+Router5 helper to determine a transition path.
+
+```
+npm install --save router5.transition-path
+```
---
+++ test/main.js
@@ -0,0 +1,35 @@
+require('should');
+require('mocha');
+var transitionPath = require('../');
+
+describe('router5.transition-path', function () {
+    it('should return a transition path with from null state', function () {
+        transitionPath({name: 'a.b.c', params: {}}, null).should.eql({
+            intersection: '',
+            toActivate: ['a', 'a.b', 'a.b.c'],
+            toDeactivate: []
+        });
+    });
+
+    it('should return transition path between two states', function () {
+        transitionPath(
+            {name: 'a.b.c.d', params: {}},
+            {name: 'a.b.e.f', params: {}}
+        ).should.eql({
+            intersection: 'a.b',
+            toActivate: ['a.b.c', 'a.b.c.d'],
+            toDeactivate: ['a.b.e.f', 'a.b.e']
+        });
+    });
+
+    it('should return transition path two states with same name but different params', function () {
+        transitionPath(
+            {name: 'a.b', params: {id: 2}},
+            {name: 'a.b', params: {id: 1}}
+        ).should.eql({
+            intersection: 'a',
+            toActivate: ['a.b'],
+            toDeactivate: ['a.b']
+        });
+    });
+});
