70be347734b13b37c5d72912571470a08ba34e80
Thomas Roch <thomas.c.roch@gmail.com>
2015-12-01 01:00:39 +0000

    chore: v2.0.1

--- dist/commonjs/index.js
+++ dist/commonjs/index.js
@@ -68,7 +68,9 @@ function transitionPath(toState, fromState) {
     }
 
     var i = undefined;
-    if (!fromState || toState.name === fromState.name && (!toState._meta || !fromState._meta)) {
+    if (!fromState) {
+        i = 0;
+    } else if (!fromState || toState.name === fromState.name && (!toState._meta || !fromState._meta)) {
         console.log('[router5.transition-path] Some states are missing metadata, reloading all segments');
         i = 0;
     } else {
--- dist/umd/index.js
+++ dist/umd/index.js
@@ -78,7 +78,9 @@
         }
 
         var i = undefined;
-        if (!fromState || toState.name === fromState.name && (!toState._meta || !fromState._meta)) {
+        if (!fromState) {
+            i = 0;
+        } else if (!fromState || toState.name === fromState.name && (!toState._meta || !fromState._meta)) {
             console.log('[router5.transition-path] Some states are missing metadata, reloading all segments');
             i = 0;
         } else {
