26783c4f4a780c5d934caacd40136318ce18ca6f
Thomas Roch <thomas.c.roch@gmail.com>
2015-12-01 00:51:17 +0000

    chore: 2.0.0

--- dist/umd/index.js
+++ dist/umd/index.js
@@ -13,28 +13,81 @@
 })(this, function (exports, module) {
     'use strict';
 
-    function transitionPath(toState, fromState) {
-        function nameToIDs(name) {
-            return name.split('.').reduce(function (ids, name) {
-                return ids.concat(ids.length ? ids[ids.length - 1] + '.' + name : name);
-            }, []);
-        }
+    function nameToIDs(name) {
+        return name.split('.').reduce(function (ids, name) {
+            return ids.concat(ids.length ? ids[ids.length - 1] + '.' + name : name);
+        }, []);
+    }
 
-        var i = undefined;
+    function extractSegmentParams(name, state) {
+        if (!state._meta || !state._meta[name]) return {};
+
+        return Object.keys(state._meta[name]).reduce(function (params, p) {
+            params[p] = state.params[p];
+            return params;
+        }, {});
+    }
+
+    function transitionPath(toState, fromState) {
         var fromStateIds = fromState ? nameToIDs(fromState.name) : [];
         var toStateIds = nameToIDs(toState.name);
         var maxI = Math.min(fromStateIds.length, toStateIds.length);
 
-        if (fromState && fromState.name === toState.name) {
-            i = Math.max(maxI - 1, 0);
-        } else {
+        function pointOfDifference() {
+            var i = undefined;
+
+            var _loop = function () {
+                var left = fromStateIds[i];
+                var right = toStateIds[i];
+
+                if (left !== right) return {
+                        v: i
+                    };
+
+                var leftParams = extractSegmentParams(left, toState);
+                var rightParams = extractSegmentParams(right, fromState);
+
+                if (leftParams.length !== rightParams.length) return {
+                        v: i
+                    };
+                if (leftParams.length === 0) return 'continue';
+
+                var different = Object.keys(leftParams).some(function (p) {
+                    return rightParams[p] !== leftParams[p];
+                });
+                if (different) {
+                    return {
+                        v: i
+                    };
+                }
+            };
+
             for (i = 0; i < maxI; i += 1) {
-                if (fromStateIds[i] !== toStateIds[i]) break;
+                var _ret = _loop();
+
+                switch (_ret) {
+                    case 'continue':
+                        continue;
+
+                    default:
+                        if (typeof _ret === 'object') return _ret.v;
+                }
             }
+
+            return i;
+        }
+
+        var i = undefined;
+        if (!fromState || toState.name === fromState.name && (!toState._meta || !fromState._meta)) {
+            console.log('[router5.transition-path] Some states are missing metadata, reloading all segments');
+            i = 0;
+        } else {
+            i = pointOfDifference();
         }
 
         var toDeactivate = fromStateIds.slice(i).reverse();
         var toActivate = toStateIds.slice(i);
+
         var intersection = fromState && i > 0 ? fromStateIds[i - 1] : '';
 
         return {
