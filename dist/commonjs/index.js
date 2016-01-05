de2cd3656169bbaf8ba076bd06c339b0e06d78e8
Thomas Roch <thomas.c.roch@gmail.com>
2016-01-05 08:31:45 +0000

    chore: v2.0.3

--- dist/commonjs/index.js
+++ dist/commonjs/index.js
@@ -1,3 +1,11 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
+function _typeof(obj) { return obj && typeof Symbol !== "undefined" && obj.constructor === Symbol ? "symbol" : typeof obj; }
+
 function nameToIDs(name) {
     return name.split('.').reduce(function (ids, name) {
         return ids.concat(ids.length ? ids[ids.length - 1] + '.' + name : name);
@@ -7,41 +15,62 @@ function nameToIDs(name) {
 function extractSegmentParams(name, state) {
     if (!state._meta || !state._meta[name]) return {};
 
-    return Object.keys(state._meta[name]).reduce((params, p) => {
+    return Object.keys(state._meta[name]).reduce(function (params, p) {
         params[p] = state.params[p];
         return params;
     }, {});
 }
 
 function transitionPath(toState, fromState) {
-    const fromStateIds = fromState ? nameToIDs(fromState.name) : [];
-    const toStateIds = nameToIDs(toState.name);
-    const maxI = Math.min(fromStateIds.length, toStateIds.length);
+    var fromStateIds = fromState ? nameToIDs(fromState.name) : [];
+    var toStateIds = nameToIDs(toState.name);
+    var maxI = Math.min(fromStateIds.length, toStateIds.length);
 
     function pointOfDifference() {
-        let i;
-        for (i = 0; i < maxI; i += 1) {
-            const left = fromStateIds[i];
-            const right = toStateIds[i];
+        var i = undefined;
 
-            if (left !== right) return i;
+        var _loop = function _loop() {
+            var left = fromStateIds[i];
+            var right = toStateIds[i];
 
-            const leftParams = extractSegmentParams(left, toState);
-            const rightParams = extractSegmentParams(right, fromState);
+            if (left !== right) return {
+                    v: i
+                };
 
-            if (leftParams.length !== rightParams.length) return i;
-            if (leftParams.length === 0) continue;
+            var leftParams = extractSegmentParams(left, toState);
+            var rightParams = extractSegmentParams(right, fromState);
 
-            const different = Object.keys(leftParams).some(p => rightParams[p] !== leftParams[p]);
+            if (leftParams.length !== rightParams.length) return {
+                    v: i
+                };
+            if (leftParams.length === 0) return 'continue';
+
+            var different = Object.keys(leftParams).some(function (p) {
+                return rightParams[p] !== leftParams[p];
+            });
             if (different) {
-                return i;
+                return {
+                    v: i
+                };
+            }
+        };
+
+        for (i = 0; i < maxI; i += 1) {
+            var _ret = _loop();
+
+            switch (_ret) {
+                case 'continue':
+                    continue;
+
+                default:
+                    if ((typeof _ret === 'undefined' ? 'undefined' : _typeof(_ret)) === "object") return _ret.v;
             }
         }
 
         return i;
     }
 
-    let i;
+    var i = undefined;
     if (!fromState) {
         i = 0;
     } else if (!fromState || toState.name === fromState.name && (!toState._meta || !fromState._meta)) {
@@ -51,16 +80,17 @@ function transitionPath(toState, fromState) {
         i = pointOfDifference();
     }
 
-    const toDeactivate = fromStateIds.slice(i).reverse();
-    const toActivate = toStateIds.slice(i);
+    var toDeactivate = fromStateIds.slice(i).reverse();
+    var toActivate = toStateIds.slice(i);
 
-    const intersection = fromState && i > 0 ? fromStateIds[i - 1] : '';
+    var intersection = fromState && i > 0 ? fromStateIds[i - 1] : '';
 
     return {
-        intersection,
-        toDeactivate,
-        toActivate
+        intersection: intersection,
+        toDeactivate: toDeactivate,
+        toActivate: toActivate
     };
 }
 
-export default transitionPath;
+exports.default = transitionPath;
+module.exports = exports['default'];
